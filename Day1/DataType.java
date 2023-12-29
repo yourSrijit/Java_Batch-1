b8ea822a3383b4d1195a4f3e1160684298f1d4bf
Srijit Bera <berasrijit02@gmail.com>
2023-12-29 21:27:58 +0530

    Add files via upload

---
+++ Day1/DataType.java
@@ -0,0 +1,25 @@
+package Day1;
+
+public class DataType {
+    public static void main(String []args){
+        System.out.println("Hello world");
+        //two type of data type 1. Primitive(predefine) 2. Non-primitive(user Defiene)
+//       int(4 byte) float(4 byte) byte(2 byte) boolean(1 byte) char(2 byte) long(8 byte) short(2 byte) double(8 byte)
+
+//        Non primitive
+//        array
+//        string
+
+        boolean isAdult=true;
+        if(isAdult){
+            System.out.println("Eligible for vote");
+        }
+        else{
+            System.out.println("Not Eligible for vote");
+        }
+
+        char a='b';
+        long  b=556465455;
+        double c=8.5;
+    }
+}
---
+++ Day1/Operators.java
@@ -0,0 +1,58 @@
+package Day1;
+
+import java.awt.*;
+
+public class Operators {
+    public static void main(String[] args) {
+        //Arithmetic operator
+//        int a=10+/*20;
+
+//        Relation operator
+        int myAge=25;
+//        if(myAge <20){
+//            System.out.println("You are under age");
+//        }
+//        if(myAge ==30){
+//            System.out.println("You are over age");
+//        }
+//        if(myAge != 50){
+//            System.out.print("Your age not 50");
+//        }
+//        if(myAge >=30){
+//            System.out.println("You are over age cause you are above 30");
+//        }
+//        if(myAge > 20 && myAge<30){
+//            System.out.println("You are eligible");
+//        }
+//        boolean isPassport=false;
+//        // true && false
+//        if(myAge >18 && isPassport){
+//            System.out.println("You are eligible");
+//        }
+//    Assignment Operator
+//        int k=12;
+//        k/=10; // k=k%12  / %
+//        System.out.println(k);
+
+        //Shift operator
+//        int p=9;
+//        int q=p<<2; // 9/4=2
+//        System.out.println(q);
+
+
+//        Bitwise operator
+        System.out.println(9^12);
+
+//        boolean c=false;
+//        if(myAge>18){
+//            c=true;
+//        }
+//        else {
+//            c=false;
+//        }
+
+        boolean c = false;
+        boolean result = c ? myAge > 18 : false;
+
+    }
+}
